1ce309df7c4da056f8552fa6508aae310b4aa565
Edward <min.wook.song@vanderbilt.edu>
2025-10-20 19:00:46 -0500

    [Bronze I] Title: 애너그램, Time: 32 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Bronze/6996. 애너그램/애너그램.py
@@ -0,0 +1,11 @@
+loop = int(input())
+
+for i in range(loop):
+    a,b = map(str, input().split())
+    x = sorted(list(a))
+    y = sorted(list(b))
+    
+    if x==y:
+        print("%s & %s are anagrams." %(a,b))
+    else:
+        print("%s & %s are NOT anagrams." %(a,b))
